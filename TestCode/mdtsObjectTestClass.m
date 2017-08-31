cafd08987fde7e5a98f2b5e34cd9aa7b5f61b777
Thomas Grandl <thomas.grandl@unileoben.ac.at>
2017-08-31 15:52:57 +0200

    Initial Commit on new GitLab Server

---
+++ TestCode/mdtsObjectTestClass.m
@@ -0,0 +1,81 @@
+classdef mdtsObjectTestClass < matlab.unittest.TestCase
+    %
+    % Description : Test the mdtsObject class
+    %
+    % Author :
+    %    Paul O'Leary
+    %    Roland Ritt
+    %    Thomas Grandl
+    %
+    % History :
+    % \change{1.0}{31-Aug-2017}{Original}
+    %
+    % --------------------------------------------------
+    % (c) 2017, Paul O'Leary
+    % Chair of Automation, University of Leoben, Austria
+    % email: automation@unileoben.ac.at
+    % url: automation.unileoben.ac.at
+    % --------------------------------------------------
+    %
+    
+    properties
+    end
+    
+    methods (Test)
+        
+        function testConstructor(testCase)
+            
+            ts = duration(0, 0, 0, 50);
+            time = [datenum(datetime(2017, 7, 31, 14, 3, 3, 123 + 0 * seconds(ts)));
+                    datenum(datetime(2017, 7, 31, 14, 3, 3, 123 + 1 * seconds(ts)));
+                    datenum(datetime(2017, 7, 31, 14, 3, 3, 123 + 2 * seconds(ts)));
+                    datenum(datetime(2017, 7, 31, 14, 3, 3, 123 + 3 * seconds(ts)))];
+            data = [9, 8;
+                    7, 6;
+                    8, 7;
+                    6, 5];
+            tags = {'Channel 1'; 'Channel2'};
+            units = {'s', 'min'};
+            name = 'TS-Test';
+            who = 'Operator';
+            when = 'Now';
+            description = {'This is a TS-Test'; 'with two text lines'};
+            comment = {'This is'; 'a comment'};
+            
+            returns = mdtsObject(time, data, tags, 'units', units, 'ts', ts, 'name', name, 'who', who, 'when', when, 'description', description, 'comment', comment);
+            returns2 = mdtsObject(time, data, tags);
+            
+            testCase.verifyEqual(returns.time, time);
+            testCase.verifyEqual(returns.data, data);
+            testCase.verifyEqual(returns.tags, tags);
+            testCase.verifyEqual(returns.units, units);
+            testCase.verifyEqual(returns.ts, ts);
+            testCase.verifyEqual(returns.uniform, 1);
+            testCase.verifyEqual(returns.name, name);
+            testCase.verifyEqual(returns.who, who);
+            testCase.verifyEqual(returns.when, when);
+            testCase.verifyEqual(returns.description, description);
+            testCase.verifyEqual(returns.comment, comment);
+            
+            testCase.verifyEqual(returns.fs, duration(0, 0, 1 / seconds(ts)));
+            testCase.verifyEqual(returns.timeRelative, time - time(1));
+            testCase.verifyEqual(returns.timeDateTime, datetime(time, 'ConvertFrom', 'datenum'));
+            
+            testCase.verifyEqual(returns2.time, time);
+            testCase.verifyEqual(returns2.data, data);
+            testCase.verifyEqual(returns2.tags, tags);
+            testCase.verifyEqual(returns2.units, {'-', '-'});
+            testCase.verifyEqual(returns2.ts, []);
+            testCase.verifyEqual(returns2.uniform, 0);
+            testCase.verifyEqual(returns2.name, 'Time Series');
+            testCase.verifyEqual(returns2.who, 'Author');
+            testCase.verifyEqual(returns2.when, 'Now');
+            testCase.verifyEqual(returns2.description, 'No descriptioni available');
+            testCase.verifyEqual(returns2.comment, 'No comment available');
+            
+            testCase.verifyEqual(returns2.timeRelative, time - time(1));
+            testCase.verifyEqual(returns2.timeDateTime, datetime(time, 'ConvertFrom', 'datenum'));
+            
+        end
+    end
+end
---
+++ mCode/mdtsObject.m
@@ -0,0 +1,60 @@
+classdef mdtsObject < CoreObject
+    %
+    % Description : Based on the core object, checks validity of input data
+    % and passes it to the core object
+    %
+    % Author :
+    %    Paul O'Leary
+    %    Roland Ritt
+    %    Thomas Grandl
+    %
+    % History :
+    % \change{1.0}{31-Aug-2017}{Original}
+    %
+    % --------------------------------------------------
+    % (c) 2017, Paul O'Leary
+    % Chair of Automation, University of Leoben, Austria
+    % email: automation@unileoben.ac.at
+    % url: automation.unileoben.ac.at
+    % --------------------------------------------------
+    %
+    
+    properties
+    end
+    
+    methods
+        
+        function obj = mdtsObject(timeIn, dataIn, tagsIn, varargin)
+            
+            p = inputParser;
+            defaultUnits = cell(1, numel(tagsIn));
+            defaultUnits(:) = {'-'};
+            defaultts = [];
+            defaultName = 'Time Series';
+            defaultWho = 'Author';
+            defaultWhen = 'Now';
+            defaultDescription = 'No descriptioni available';
+            defaultComment = 'No comment available';
+            
+            addRequired(p, 'timeIn', @(x)validateattributes(x, {'numeric'}, {'nonempty'}));
+            addRequired(p, 'dataIn', @(x)validateattributes(x, {'numeric'}, {'nonempty'}));
+            addRequired(p, 'tagsIn', @(x)validateattributes(x, {'cell'}, {'nonempty'}));
+            
+            addParameter(p, 'units', defaultUnits);
+            addParameter(p, 'ts', defaultts);
+            addParameter(p, 'name', defaultName);
+            addParameter(p, 'who', defaultWho);
+            addParameter(p, 'when', defaultWhen);
+            addParameter(p, 'description', defaultDescription);
+            addParameter(p, 'comment', defaultComment);
+            
+            parse(p, timeIn, dataIn, tagsIn, varargin{:});
+            
+            obj@CoreObject(p.Results.timeIn, p.Results.dataIn, p.Results.tagsIn, p.Results.units, p.Results.ts, p.Results.name, p.Results.who, p.Results.when, p.Results.description, p.Results.comment);
+            
+        end
+            
+    end
+    
+end
+
