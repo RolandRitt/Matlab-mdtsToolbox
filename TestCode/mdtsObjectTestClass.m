5e3dd680f6bdb9f0bdfdad2b4ba1c8453d272cd1
Thomas Grandl <thomas.grandl@unileoben.ac.at>
2017-08-31 17:45:20 +0200

    Added test cases for function 'expandDataSet'

--- TestCode/mdtsObjectTestClass.m
+++ TestCode/mdtsObjectTestClass.m
@@ -70,11 +70,60 @@ classdef mdtsObjectTestClass < matlab.unittest.TestCase
             testCase.verifyEqual(returns2.name, 'Time Series');
             testCase.verifyEqual(returns2.who, 'Author');
             testCase.verifyEqual(returns2.when, 'Now');
-            testCase.verifyEqual(returns2.description, 'No descriptioni available');
+            testCase.verifyEqual(returns2.description, 'No description available');
             testCase.verifyEqual(returns2.comment, 'No comment available');
             
             testCase.verifyEqual(returns2.timeRelative, time - time(1));
             testCase.verifyEqual(returns2.timeDateTime, datetime(time, 'ConvertFrom', 'datenum'));
+            
+        end
+        
+        function testExpandDataSet(testCase)
+            
+            ts = duration(0, 0, 0, 50);
+            time = [datenum(datetime(2017, 7, 25, 14, 3, 3, 123));
+                    datenum(datetime(2017, 7, 30, 14, 3, 3, 123));
+                    datenum(datetime(2017, 7, 31, 14, 3, 3, 123));
+                    datenum(datetime(2017, 8, 5, 14, 3, 3, 123));
+                    datenum(datetime(2017, 8, 15, 14, 3, 3, 123));
+                    datenum(datetime(2017, 9, 5, 14, 3, 3, 123))];
+            data = [9, 8, 7, 6;
+                    7, 6, 5, 4;
+                    8, 7, 6, 5;
+                    6, 5, 4, 3;
+                    4, 3, 2, 1;
+                    5, 4, 3, 2];
+            tags = {'Channel 1', 'Channel 2', 'Channel 3', 'Channel 4'};
+            units = {'s', 'min', 'elephants', 'giraffes'};
+            name = 'TS-Test';
+            who = 'Operator';
+            when = 'Now';
+            description = {'This is a TS-Test'; 'with two text lines'};
+            comment = {'This is'; 'a comment'};
+            
+            addData = [2, 3;
+                       3, 4;
+                       4, 5;
+                       5, 6;
+                       6, 7;
+                       7, 8];
+            addTags = {'AddedTag1', 'AddedTag2'};
+            
+            returns = mdtsObject(time, data, tags, 'units', units, 'ts', ts, 'name', name, 'who', who, 'when', when, 'description', description, 'comment', comment);
+            returns.expandDataSet(addData, addTags);
+            
+            testCase.verifyEqual(returns.exData, addData);
+            testCase.verifyEqual(returns.exTags, addTags);
+            
+            returns.expandDataSet(addData, addTags);
+            
+            testCase.verifyEqual(returns.exData, [addData, addData]);
+            testCase.verifyEqual(returns.exTags, [addTags, addTags]);
+                                  
+            testCase.verifyError(@()returns.expandDataSet({'No numeric dataset'}, addTags), 'expandDataSet:DataNotNumeric');
+            testCase.verifyError(@()returns.expandDataSet(addData(1 : end - 1, :), addTags), 'expandDataSet:InvalideDataSize1');
+            testCase.verifyError(@()returns.expandDataSet(addData(:, 1), addTags), 'expandDataSet:InvalideDataSize2');
+            testCase.verifyError(@()returns.expandDataSet(addData, [1, 2]), 'expandDataSet:InvalidTags');
             
         end
     end
