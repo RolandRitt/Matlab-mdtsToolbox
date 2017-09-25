d76e29496c627bd5979af9990d72d7b0ee43d7c2
Grandl <Grandl@pc-tgr-2.ia.unileoben.ac.at>
2017-09-25 14:43:24 +0200

    Initial commit.
    This abstract class defines the interface for every calculation object.

---
+++ mCode/calcObjectInterface.m
@@ -0,0 +1,31 @@
+classdef (Abstract) calcObjectInterface
+    %
+    % Description : Define the interface of calculation objects
+    %
+    % Author :
+    %    Paul O'Leary
+    %    Roland Ritt
+    %    Thomas Grandl
+    %
+    % History :
+    % \change{1.0}{25-Sep-2017}{Original}
+    %
+    % --------------------------------------------------
+    % (c) 2017, Paul O'Leary
+    % Chair of Automation, University of Leoben, Austria
+    % email: automation@unileoben.ac.at
+    % url: automation.unileoben.ac.at
+    % --------------------------------------------------
+    %
+    
+    properties
+    end
+    
+    methods (Abstract)
+        
+        result = apply(obj, inputData)
+        
+    end
+    
+end
+
