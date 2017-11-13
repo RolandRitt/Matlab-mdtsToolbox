238e2e4d64fe3fefee74d381b756a55277d0c1cf
Grandl <Grandl@pc-tgr-2.ia.unileoben.ac.at>
2017-11-13 08:19:05 +0100

    Moved all input checks from the core object to this object

--- mCode/mdtsObject.m
+++ mCode/mdtsObject.m
@@ -54,10 +54,66 @@ classdef mdtsObject < mdtsCoreObject
             
         end
         
+        function returnObject = getData(obj, varargin)
+            
+            nArguments = numel(varargin);
+    
+            if(nArguments > 2 || nArguments == 0)
+            
+                    errID = 'getData:InvalidNumberOfInputs';
+                    errMsg = 'Invalid number of input arguments!';
+                    error(errID, errMsg);
+                    
+            else
+                
+                returnObject = getData@mdtsCoreObject(obj, varargin); 
+                
+            end
+            
+        end
+        
+        function tagIndices = getTagIndices(obj, tagList)
+                       
+            correctTagInput = ismember(tagList, obj.tags);
+                       
+            if(correctTagInput)
+                
+                tagIndices = getTagIndices@mdtsCoreObject(obj, tagList); 
+                               
+            else
+                
+                notIncludedTags = tagList(~correctTagInput);
+                
+                errID = 'getTagIndices:TagNotAvailable';
+                errMsg = ['Tag(s) ', strjoin(notIncludedTags, ', '), ' not available within the data!'];
+                error(errID, errMsg);
+                
+            end
+            
+        end
+        
+        function intervalIndices = getIntervalIndices(obj, timeInterval)
+            
+            if(timeInterval(1) >= obj.time(1) && timeInterval(end) <= obj.time(end))
+                
+                intervalIndices = getIntervalIndices@mdtsCoreObject(obj, timeInterval); 
+                
+            else
+                
+                errID = 'getIntervalIndices:IntervalOutOfBoundaries';
+                errMsg = 'Specified interval exceeds the boundaries of the data!';
+                error(errID, errMsg);
+                
+            end
+            
+        end
+        
         function expandDataSet(obj, addData, addTags)
             
             timeSize = numel(obj.time);
             addTagSize = numel(addTags);
+            
+            
             
             if~isnumeric(addData)
                 
@@ -82,172 +138,178 @@ classdef mdtsObject < mdtsCoreObject
                 errID = 'expandDataSet:InvalidTags';
                 errMsg = 'The given tags are no cell array!';
                 error(errID, errMsg);
-                              
-            end
-            
-            expandDataSet@mdtsCoreObject(obj, addData, addTags); 
-            
-        end
-        
-        function obj = calc(obj, calcObj, tagNameInput, tagNameOutput)
-            
-            p = inputParser();
-            addRequired(p, 'calcObj', @(x) isa(x, 'calcObjectInterface'));
-            addRequired(p, 'tagNameInput', @(x) iscell(x) && ischar(x{1}));
-            addRequired(p, 'tagNameOutput', @(x) iscell(x) && ischar(x{1}));
-            parse(p, calcObj, tagNameInput, tagNameOutput);
-            
-            tagI = getTagIndices(obj, p.Results.tagNameInput);
-            
-            dataOut = calcObj.apply(obj.data(:, tagI));
-            
-            expandDataSet(obj, dataOut, p.Results.tagNameOutput);
-            
-        end
-        
-        function obj = convCalc(obj, varargin)
-            
-            if(numel(varargin) == 1)
                 
-                calcObjects = varargin{1};
+            elseif(checkTags(obj, addTags))
                 
-                for i = 1 : numel(calcObjects)
-                    
-                    calcObject = calcObjects(i);
-                    
-                    inputTag = calcObject.inputTag;
-                    outputTag = calcObject.outputTag;
-                    convMatrix = calcObject.convM;
-                    
-                    tagI = getTagIndices(obj, inputTag);
-                    inputVector = obj.data(:, tagI);
-                    
-                    outputVector = convCalcFct(inputVector, convMatrix);
-                    
-                    expandDataSet(obj, outputVector, outputTag);
-                    
-                end
-                
-            elseif(numel(varargin) == 3)
-                
-                inputTag = varargin{1};
-                outputTag = varargin{2};
-                
-                if(ischar(outputTag))
-                    
-                    outputTag = {outputTag};
-                    
-                end
-                
-                convMatrix = varargin{3};
-                
-                tagI = getTagIndices(obj, inputTag);
-                inputVector = obj.data(:, tagI);
-                
-                outputVector = convCalcFct(inputVector, convMatrix);
-                
-                expandDataSet(obj, outputVector, outputTag);
-                
-            else
-                
-                errID = 'mdtsObject:InvalidNumberOfInputArguments';
-                errMsg = ['Invalid number of input arguments! Input must be ',...
-                'either a convolution calculation object or inputTag, outputTag, convolutionMatrix!'];
+                errID = 'expandDataSet:NonUniqueTags';
+                errMsg = 'Given tags for data expansion are already existend in the data set!';
                 error(errID, errMsg);
                 
             end
             
-        end
-        
-        function obj = localDerivative(obj, tagNameInput, ls, noBfs)
-            
-            p = inputParser();
-            addRequired(p, 'tagNameInput', @(x) iscell(x) && ischar(x{1}));
-            addRequired(p, 'ls', @(x) isnumeric(x) && isequal(size(x),[1 1]));
-            addRequired(p, 'noBfs', @(x) isnumeric(x) && isequal(size(x),[1 1]));
-            parse(p, tagNameInput, ls, noBfs);
-            
-            L = dopD(obj.time(1 : p.Results.ls), p.Results.noBfs);
-            LDOobj = LDO(L);
-            
-            tagNameOutput = {['LD_', p.Results.tagNameInput{1}]};
-            obj = obj.calc(LDOobj, p.Results.tagNameInput, tagNameOutput);
+            expandDataSet@mdtsCoreObject(obj, addData, addTags);
             
         end
         
-        function figH = plotMulti(obj, varargin)
-            
-            % Plot channels
-            
-            p = inputParser();
-            p.KeepUnmatched=true;
-            addRequired(p, 'obj', @(x) isa(x, 'mdtsObject')); %check if input is a MDTSObject
-            addParameter(p, 'Size', [8.8,11.7], @(x)isnumeric(x)&&isvector(x)); %higth and width
-            addParameter(p, 'FontSize', 10, @isnumeric);
-            addParameter(p, 'bUseDatetime', true, @islogical);
-            parse(p, obj, varargin{:});
-            tmp = [fieldnames(p.Unmatched),struct2cell(p.Unmatched)];
-            UnmatchedArgs = reshape(tmp',[],1)';
-            
-            bDatetime = p.Results.bUseDatetime;
-            
-            tagIndices = obj.getTagIndices(obj.tags);
-            
-            figH = figureGen(p.Results.Size(1), p.Results.Size(2), p.Results.FontSize);
-            fM = FigureManager;
-            
-            if bDatetime
-                
-                [out, ph] = plotMulti(obj.timeDateTime, obj.data(:, tagIndices), 'Time', obj.tags, UnmatchedArgs{:});
-            else
-                
-                [out, ph] = plotMulti(obj.time, obj.data(:, tagIndices), 'Time', obj.tags, UnmatchedArgs{:});
-            
-            end
-                       
-            shouldAddold = fM.shouldAdd;
-            fM.shouldAdd = false; % otherwise it is tooo slow!!!  
-            title(out(1), obj.name);
-            
-            % Plot events
-                        
-            nEvents = length(keys(obj.tsEvents));
-            colors = distinguishable_colors(nEvents, {'w', get(ph(1), 'Color')});
-            
-            if bDatetime
-                
-                if nEvents
-                    
-                    indEv = 1;
-                    
-                    for key = keys(obj.tsEvents)
-                        
-                        xev = datetime(obj.tsEvents(key{1}), 'ConvertFrom', 'datenum');
-                        ph2 = plotvline(xev, 'Axes', out, 'Color', colors(indEv,:));
-                        indEv = indEv + 1;
-                        ph = [ph, ph2];
-                        
-                    end
-                end
-            else
-                if nEvents
-                    
-                    indEv = 1;
-                    
-                    for key = keys(obj.tsEvents)
-                        
-                        xev = obj.tsEvents(key{1});
-                        ph2 = plotvline(xev, 'Axes', out, 'Color', colors(indEv,:));
-                        indEv = indEv + 1;
-                        ph = [ph, ph2];
-                        
-                    end
-                end
-            end
-            
-            fM.shouldAdd = shouldAddold;
-            
-        end
+%         function obj = calc(obj, calcObj, tagNameInput, tagNameOutput)
+%             
+%             p = inputParser();
+%             addRequired(p, 'calcObj', @(x) isa(x, 'calcObjectInterface'));
+%             addRequired(p, 'tagNameInput', @(x) iscell(x) && ischar(x{1}));
+%             addRequired(p, 'tagNameOutput', @(x) iscell(x) && ischar(x{1}));
+%             parse(p, calcObj, tagNameInput, tagNameOutput);
+%             
+%             tagI = getTagIndices(obj, p.Results.tagNameInput);
+%             
+%             dataOut = calcObj.apply(obj.data(:, tagI));
+%             
+%             expandDataSet(obj, dataOut, p.Results.tagNameOutput);
+%             
+%         end
+%         
+%         function obj = convCalc(obj, varargin)
+%             
+%             if(numel(varargin) == 1)
+%                 
+%                 calcObjects = varargin{1};
+%                 
+%                 for i = 1 : numel(calcObjects)
+%                     
+%                     calcObject = calcObjects(i);
+%                     
+%                     inputTag = calcObject.inputTag;
+%                     outputTag = calcObject.outputTag;
+%                     convMatrix = calcObject.convM;
+%                     
+%                     tagI = getTagIndices(obj, inputTag);
+%                     inputVector = obj.data(:, tagI);
+%                     
+%                     outputVector = convCalcFct(inputVector, convMatrix);
+%                     
+%                     expandDataSet(obj, outputVector, outputTag);
+%                     
+%                 end
+%                 
+%             elseif(numel(varargin) == 3)
+%                 
+%                 inputTag = varargin{1};
+%                 outputTag = varargin{2};
+%                 
+%                 if(ischar(outputTag))
+%                     
+%                     outputTag = {outputTag};
+%                     
+%                 end
+%                 
+%                 convMatrix = varargin{3};
+%                 
+%                 tagI = getTagIndices(obj, inputTag);
+%                 inputVector = obj.data(:, tagI);
+%                 
+%                 outputVector = convCalcFct(inputVector, convMatrix);
+%                 
+%                 expandDataSet(obj, outputVector, outputTag);
+%                 
+%             else
+%                 
+%                 errID = 'mdtsObject:InvalidNumberOfInputArguments';
+%                 errMsg = ['Invalid number of input arguments! Input must be ',...
+%                 'either a convolution calculation object or inputTag, outputTag, convolutionMatrix!'];
+%                 error(errID, errMsg);
+%                 
+%             end
+%             
+%         end
+%         
+%         function obj = localDerivative(obj, tagNameInput, ls, noBfs)
+%             
+%             p = inputParser();
+%             addRequired(p, 'tagNameInput', @(x) iscell(x) && ischar(x{1}));
+%             addRequired(p, 'ls', @(x) isnumeric(x) && isequal(size(x),[1 1]));
+%             addRequired(p, 'noBfs', @(x) isnumeric(x) && isequal(size(x),[1 1]));
+%             parse(p, tagNameInput, ls, noBfs);
+%             
+%             L = dopD(obj.time(1 : p.Results.ls), p.Results.noBfs);
+%             LDOobj = LDO(L);
+%             
+%             tagNameOutput = {['LD_', p.Results.tagNameInput{1}]};
+%             obj = obj.calc(LDOobj, p.Results.tagNameInput, tagNameOutput);
+%             
+%         end
+%         
+%         function figH = plotMulti(obj, varargin)
+%             
+%             % Plot channels
+%             
+%             p = inputParser();
+%             p.KeepUnmatched=true;
+%             addRequired(p, 'obj', @(x) isa(x, 'mdtsObject')); %check if input is a MDTSObject
+%             addParameter(p, 'Size', [8.8,11.7], @(x)isnumeric(x)&&isvector(x)); %higth and width
+%             addParameter(p, 'FontSize', 10, @isnumeric);
+%             addParameter(p, 'bUseDatetime', true, @islogical);
+%             parse(p, obj, varargin{:});
+%             tmp = [fieldnames(p.Unmatched),struct2cell(p.Unmatched)];
+%             UnmatchedArgs = reshape(tmp',[],1)';
+%             
+%             bDatetime = p.Results.bUseDatetime;
+%             
+%             tagIndices = obj.getTagIndices(obj.tags);
+%             
+%             figH = figureGen(p.Results.Size(1), p.Results.Size(2), p.Results.FontSize);
+%             fM = FigureManager;
+%             
+%             if bDatetime
+%                 
+%                 [out, ph] = plotMulti(obj.timeDateTime, obj.data(:, tagIndices), 'Time', obj.tags, UnmatchedArgs{:});
+%             else
+%                 
+%                 [out, ph] = plotMulti(obj.time, obj.data(:, tagIndices), 'Time', obj.tags, UnmatchedArgs{:});
+%             
+%             end
+%                        
+%             shouldAddold = fM.shouldAdd;
+%             fM.shouldAdd = false; % otherwise it is tooo slow!!!  
+%             title(out(1), obj.name);
+%             
+%             % Plot events
+%                         
+%             nEvents = length(keys(obj.tsEvents));
+%             colors = distinguishable_colors(nEvents, {'w', get(ph(1), 'Color')});
+%             
+%             if bDatetime
+%                 
+%                 if nEvents
+%                     
+%                     indEv = 1;
+%                     
+%                     for key = keys(obj.tsEvents)
+%                         
+%                         xev = datetime(obj.tsEvents(key{1}), 'ConvertFrom', 'datenum');
+%                         ph2 = plotvline(xev, 'Axes', out, 'Color', colors(indEv,:));
+%                         indEv = indEv + 1;
+%                         ph = [ph, ph2];
+%                         
+%                     end
+%                 end
+%             else
+%                 if nEvents
+%                     
+%                     indEv = 1;
+%                     
+%                     for key = keys(obj.tsEvents)
+%                         
+%                         xev = obj.tsEvents(key{1});
+%                         ph2 = plotvline(xev, 'Axes', out, 'Color', colors(indEv,:));
+%                         indEv = indEv + 1;
+%                         ph = [ph, ph2];
+%                         
+%                     end
+%                 end
+%             end
+%             
+%             fM.shouldAdd = shouldAddold;
+%             
+%         end
         
         function obj = addEvent(obj, name, x, bInd)
             % function to add events at defined x positions
