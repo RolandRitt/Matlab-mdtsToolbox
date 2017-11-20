88817b44597c74841bf017ee5c371813d1bea51f
Grandl <Grandl@pc-tgr-2.ia.unileoben.ac.at>
2017-11-20 13:03:58 +0100

    'Tidied up' code.
    Removed obsolete test cases, updated comments, added test some test cases.

--- TestCode/mdtsObjectTestClass.m
+++ TestCode/mdtsObjectTestClass.m
@@ -78,6 +78,45 @@ classdef mdtsObjectTestClass < matlab.unittest.TestCase
             
         end
         
+                function testEqualDistribution(testCase)
+            
+            ts = duration(0, 0, 0, 50);
+            time = [datenum(datetime(2017, 7, 31, 14, 3, 3, 123 + 0 * seconds(ts)));
+                    datenum(datetime(2017, 7, 31, 14, 3, 3, 123 + 1 * seconds(ts)));
+                    datenum(datetime(2017, 7, 31, 14, 3, 3, 123 + 2 * seconds(ts)));
+                    datenum(datetime(2017, 7, 31, 14, 3, 3, 123 + 3 * seconds(ts)))];
+            data = [9, 8;
+                    7, 6;
+                    8, 7;
+                    6, 5];
+            tags = {'Channel 1'; 'Channel2'};
+            units = {'s', 'min'};
+            name = 'TS-Test';
+            who = 'Operator';
+            when = 'Now';
+            description = {'This is a TS-Test'; 'with two text lines'};
+            comment = {'This is'; 'a comment'};
+            
+            returns = mdtsCoreObject(time, data, tags, units, ts, name, who, when, description, comment);
+            
+            testCase.verifyEqual(returns.time, time);
+            testCase.verifyEqual(returns.data, data);
+            testCase.verifyEqual(returns.tags, tags);
+            testCase.verifyEqual(returns.units, units);
+            testCase.verifyEqual(returns.ts, ts);
+            testCase.verifyEqual(returns.uniform, 1);
+            testCase.verifyEqual(returns.name, name);
+            testCase.verifyEqual(returns.who, who);
+            testCase.verifyEqual(returns.when, when);
+            testCase.verifyEqual(returns.description, description);
+            testCase.verifyEqual(returns.comment, comment);
+            
+            testCase.verifyEqual(returns.fs, 1 / seconds(ts));
+            testCase.verifyEqual(returns.timeRelative, time - time(1));
+            testCase.verifyEqual(returns.timeDateTime, datetime(time, 'ConvertFrom', 'datenum'));
+            
+        end
+        
         function testsubsref(testCase)
             
             ts = duration(0, 0, 0, 50);
@@ -503,46 +542,46 @@ classdef mdtsObjectTestClass < matlab.unittest.TestCase
 %             
 %         end
         
-        function testAddEvent(testCase)
-            
-            ts = duration(0, 0, 0, 50);
-            time = [datenum(datetime(2017, 7, 25, 14, 3, 3, 123));
-                    datenum(datetime(2017, 7, 30, 14, 3, 3, 123));
-                    datenum(datetime(2017, 7, 31, 14, 3, 3, 123));
-                    datenum(datetime(2017, 8, 5, 14, 3, 3, 123));
-                    datenum(datetime(2017, 8, 15, 14, 3, 3, 123));
-                    datenum(datetime(2017, 9, 5, 14, 3, 3, 123))];
-            data = [9, 8, 7, 6;
-                    7, 6, 5, 4;
-                    8, 7, 6, 5;
-                    6, 5, 4, 3;
-                    4, 3, 2, 1;
-                    5, 4, 3, 2];
-            tags = {'Channel 1', 'Channel 2', 'Channel 3', 'Channel 4'};
-            units = {'s', 'min', 'elephants', 'giraffes'};
-            name = 'TS-Test';
-            who = 'Operator';
-            when = 'Now';
-            description = {'This is a TS-Test'; 'with two text lines'};
-            comment = {'This is'; 'a comment'};
-            
-            eventName1 = 'MyEvent1';
-            eventName2 = 'MyEvent2';
-            eventName3 = 'MyEvent3';
-            eventTime1 = time(3);
-            eventTime2 = 5;
-            eventTime3 = datetime(2017, 9, 5, 14, 3, 3, 123);
-            
-            returns = mdtsObject(time, data, tags, 'units', units, 'ts', ts, 'name', name, 'who', who, 'when', when, 'description', description, 'comment', comment);
-            
-            returns.addEvent(eventName1, eventTime1);
-            returns.addEvent(eventName2, eventTime2, 1);
-            returns.addEvent(eventName3, eventTime3);
-            
-            testCase.verifyEqual(eventTime1, returns.tsEvents(eventName1));
-            testCase.verifyEqual(time(eventTime2), returns.tsEvents(eventName2));
-            testCase.verifyEqual(datenum(eventTime3), returns.tsEvents(eventName3));
-            
-        end
+%         function testAddEvent(testCase)
+%             
+%             ts = duration(0, 0, 0, 50);
+%             time = [datenum(datetime(2017, 7, 25, 14, 3, 3, 123));
+%                     datenum(datetime(2017, 7, 30, 14, 3, 3, 123));
+%                     datenum(datetime(2017, 7, 31, 14, 3, 3, 123));
+%                     datenum(datetime(2017, 8, 5, 14, 3, 3, 123));
+%                     datenum(datetime(2017, 8, 15, 14, 3, 3, 123));
+%                     datenum(datetime(2017, 9, 5, 14, 3, 3, 123))];
+%             data = [9, 8, 7, 6;
+%                     7, 6, 5, 4;
+%                     8, 7, 6, 5;
+%                     6, 5, 4, 3;
+%                     4, 3, 2, 1;
+%                     5, 4, 3, 2];
+%             tags = {'Channel 1', 'Channel 2', 'Channel 3', 'Channel 4'};
+%             units = {'s', 'min', 'elephants', 'giraffes'};
+%             name = 'TS-Test';
+%             who = 'Operator';
+%             when = 'Now';
+%             description = {'This is a TS-Test'; 'with two text lines'};
+%             comment = {'This is'; 'a comment'};
+%             
+%             eventName1 = 'MyEvent1';
+%             eventName2 = 'MyEvent2';
+%             eventName3 = 'MyEvent3';
+%             eventTime1 = time(3);
+%             eventTime2 = 5;
+%             eventTime3 = datetime(2017, 9, 5, 14, 3, 3, 123);
+%             
+%             returns = mdtsObject(time, data, tags, 'units', units, 'ts', ts, 'name', name, 'who', who, 'when', when, 'description', description, 'comment', comment);
+%             
+%             returns.addEvent(eventName1, eventTime1);
+%             returns.addEvent(eventName2, eventTime2, 1);
+%             returns.addEvent(eventName3, eventTime3);
+%             
+%             testCase.verifyEqual(eventTime1, returns.tsEvents(eventName1));
+%             testCase.verifyEqual(time(eventTime2), returns.tsEvents(eventName2));
+%             testCase.verifyEqual(datenum(eventTime3), returns.tsEvents(eventName3));
+%             
+%         end
     end
 end
