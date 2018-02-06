42e8e641578cb325fb795379d2ad946fb5ae578c
Grandl <Grandl@pc-tgr-2.ia.unileoben.ac.at>
2018-02-06 10:59:18 +0100

    Completely renewed mcla application process to increase performance.

--- mCode/applyMCLA.m
+++ mCode/applyMCLA.m
@@ -79,34 +79,99 @@ for i = 1 : nCombinations
     
 end
 
-channelLength = numel(symbRepObjectsList{1}.symbRepVec);
-allSymbols = cell(channelLength, nSymbRepObjects);
+% channelLength = numel(symbRepObjectsList{1}.symbRepVec);
+% allSymbols = cell(channelLength, nSymbRepObjects);
+% 
+% for i = 1 : nSymbRepObjects
+%     
+%     tempSymbols = cellstr(symbRepObjectsList{i}.symbRepVec);
+%     addedBrackets = cellfun(@(x) ['{', x, '}'], tempSymbols, 'UniformOutput', false);
+%     removedBrackets = cellfun(@(x) strrep(x, '{[', '['), addedBrackets, 'UniformOutput', false);
+%     removedBrackets = cellfun(@(x) strrep(x, '{(', '('), removedBrackets, 'UniformOutput', false);
+%     removedBrackets = cellfun(@(x) strrep(x, ']}', ']'), removedBrackets, 'UniformOutput', false);
+%     removedBrackets = cellfun(@(x) strrep(x, ')}', ')'), removedBrackets, 'UniformOutput', false);
+%     allSymbols(:, i) = removedBrackets;
+%     
+% end
+% 
+% mergedSymbols = cell(channelLength, 1);
+% 
+% for i = 1 : channelLength
+%     
+%     tempSymbol = join(allSymbols(i, :), '');
+%     mergedSymbols{i} = ['(', tempSymbol{1}, ')'];
+%     
+% end
+% 
+% numRepr = int32(categorical(mergedSymbols));
+% symbolChange = [true; diff(numRepr) ~= 0];
+% mclaSymbols = categorical(mergedSymbols(symbolChange), allCombinations);
+% mclaDurations = diff(find([symbolChange; 1]));
+% 
+% mclaSymbRepObject = SymbRepObject(mclaDurations, mclaSymbols);
+
+initialSymbolLength = sum(cell2mat(cellfun(@(x) numel(x.symbols), symbRepObjectsList, 'UniformOutput', false)));
+maxNSymbols = max(cell2mat(cellfun(@(x) numel(x.symbols), symbRepObjectsList, 'UniformOutput', false)));
+
+allSymbols = cell(maxNSymbols + 1, nSymbRepObjects);
+allDurations = Inf(maxNSymbols + 1, nSymbRepObjects);
 
 for i = 1 : nSymbRepObjects
+       
+    tempSymbols = cellstr(symbRepObjectsList{i}.symbols);
+    addedBrackets2 = cellfun(@(x) ['{', x, '}'], tempSymbols, 'UniformOutput', false);
+    removedBrackets2 = cellfun(@(x) strrep(x, '{[', '['), addedBrackets2, 'UniformOutput', false);
+    removedBrackets2 = cellfun(@(x) strrep(x, '{(', '('), removedBrackets2, 'UniformOutput', false);
+    removedBrackets2 = cellfun(@(x) strrep(x, ']}', ']'), removedBrackets2, 'UniformOutput', false);
+    removedBrackets2 = cellfun(@(x) strrep(x, ')}', ')'), removedBrackets2, 'UniformOutput', false);
+    allSymbols(1 : numel(removedBrackets2), i) = removedBrackets2;
     
-    tempSymbols = cellstr(symbRepObjectsList{i}.symbRepVec);
-    addedBrackets = cellfun(@(x) ['{', x, '}'], tempSymbols, 'UniformOutput', false);
-    removedBrackets = cellfun(@(x) strrep(x, '{[', '['), addedBrackets, 'UniformOutput', false);
-    removedBrackets = cellfun(@(x) strrep(x, '{(', '('), removedBrackets, 'UniformOutput', false);
-    removedBrackets = cellfun(@(x) strrep(x, ']}', ']'), removedBrackets, 'UniformOutput', false);
-    removedBrackets = cellfun(@(x) strrep(x, ')}', ')'), removedBrackets, 'UniformOutput', false);
-    allSymbols(:, i) = removedBrackets;
-    
-end
-
-mergedSymbols = cell(channelLength, 1);
-
-for i = 1 : channelLength
-    
-    tempSymbol = join(allSymbols(i, :), '');
-    mergedSymbols{i} = ['(', tempSymbol{1}, ')'];
+    allDurations(1 : numel(symbRepObjectsList{i}.durations), i) = symbRepObjectsList{i}.durations;
     
 end
 
-numRepr = int32(categorical(mergedSymbols));
-symbolChange = [true; diff(numRepr) ~= 0];
-mclaSymbols = categorical(mergedSymbols(symbolChange), allCombinations);
-mclaDurations = diff(find([symbolChange; 1]));
+initialSymbolCellArray = cell(initialSymbolLength, 1);
+initialDurationsArray = zeros(initialSymbolLength, 1);
+
+ind = ones(1, nSymbRepObjects);
+matrixSize = size(allDurations);
+
+atEnd = false;
+iter = 1;
+
+while ~atEnd
+    
+    tempInds = sub2ind(matrixSize, ind, 1 : nSymbRepObjects);
+    
+    stepSize = min(allDurations(tempInds));
+        
+    if(stepSize == Inf)
+        
+        atEnd = true;
+        
+    else       
+        
+        %tempSymbol = strjoin(allSymbols(tempInds), '');
+        %initialSymbolCellArray{iter} = ['(', tempSymbol, ')'];        
+        tempSymbol = join([{'('}, allSymbols(tempInds), {')'}], '');
+        initialSymbolCellArray(iter) = tempSymbol;   
+        
+        steppingIndex = find(allDurations(tempInds) == stepSize);
+        
+        initialDurationsArray(iter) = stepSize;
+        
+        allDurations(tempInds) = allDurations(tempInds) - stepSize;        
+                
+        ind(steppingIndex) = ind(steppingIndex) + 1;
+        
+        iter = iter + 1;
+        
+    end
+    
+end
+
+mclaSymbols = categorical(initialSymbolCellArray(~cellfun(@isempty, initialSymbolCellArray)), allCombinations);
+mclaDurations = initialDurationsArray(initialDurationsArray ~= 0);
 
 mclaSymbRepObject = SymbRepObject(mclaDurations, mclaSymbols);
 
