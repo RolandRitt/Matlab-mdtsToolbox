0432292284f92838dcb00ce3e00cd7c503fd98a7
Grandl <Grandl@pc-tgr-2.ia.unileoben.ac.at>
2018-05-08 17:12:24 +0200

    Added all files which have not been included yet

---
+++ development/LDOdef2MTX.m
@@ -0,0 +1,40 @@
+function ldoStruct = LDOdef2MTX()
+% convolution, local derivative
+%
+% Purpose : Generate a convolution matrix for a LDO
+%
+% Syntax :
+%   ldoStruct = LDOdef2MTX()
+%
+% Input Parameters :
+%   
+%   
+% Return Parameters :
+%   ldoStruct : Structure containing all information of the local
+%   derivative, including the convolution matrix
+%   
+% Description : 
+%   Generates a strucutre with all information about the local derivative.
+%   The local derivative will be executed as convolution. The sturct
+%   therefore contains the corresponding convolution matrix.
+%
+% Author :
+%   Paul O'Leary
+%   Roland Ritt
+%   Thomas Grandl
+%
+% History :
+% \change{1.0}{16-Nov-2017}{Original version}
+%
+% --------------------------------------------------------
+% (c) 2017 Paul O'Leary,
+% Chair of Automation, University of Leoben, Leoben, Austria
+% email: office@harkeroleary.org,
+% url: www.harkeroleary.org
+% --------------------------------------------------------
+%
+%%
+
+
+end
+
