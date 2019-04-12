ef53ab07322b0067f30c42743d4ed0599ab2db1f
Roland Ritt <roland.ritt@unileoben.ac.at>
2019-04-12 08:28:50 +0200

    initial commit

---
+++ SymbolicRepresentation/mCode/isHierarchicalSymRep.m
@@ -0,0 +1,48 @@
+function bTrue = isHierarchicalSymRep(hierarchicalSymbolicData)
+% <keywords>
+%
+% Purpose : verify if given input is a hierarchicalSymbolicData used in
+% TS_SAX_Multi, TS_SAX
+%
+% Syntax :
+%
+% Input Parameters :
+%   hierarchicalSymbolicData := a hierarical representation of
+%
+% Return Parameters :
+%   bTrue := returns true if input is valid hierachical symbolic representation
+%
+% Description :
+%
+% Author :
+%    Roland Ritt
+%
+% History :
+% \change{1.0}{19-Dec-2017}{Original}
+%
+% --------------------------------------------------
+% (c) 2017, Roland Ritt
+% Chair of Automation, University of Leoben, Austria
+% email: automation@unileoben.ac.at
+% url: automation.unileoben.ac.at
+% --------------------------------------------------
+%
+%%
+
+bTrue = false;
+try
+    if ~iscell(hierarchicalSymbolicData)
+        return;
+    end
+    
+    
+    for i=1:length(hierarchicalSymbolicData)
+        if ~isa(hierarchicalSymbolicData{i}, 'SymbRepObject')
+            return;
+        end
+    end
+
+    bTrue = true;
+catch
+    bTrue = false;
+end
